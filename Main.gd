5b49d0bc64be7967ab4a5a3845ec0ae7d13f9cef
Bjorn Hjortsberg <bjorn.hjortsberg@gmail.com>
2020-01-04 14:57:28 +0100

    Add a top banner and interstitial on game exit
    Hide Ad when playing
    
    Show only Ad unit id for test ads

--- Main.gd
+++ Main.gd
@@ -5,13 +5,23 @@ extends Node2D
 # var b = "text"
 onready var main = get_node("/root/main_vars")
 
+var admob = null
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# AdMod
+	if (Engine.has_singleton("AdMob")):
+		admob = Engine.get_singleton("AdMob")
+		admob.init(false, get_instance_id())
+		admob.loadBanner("ca-app-pub-3940256099942544/6300978111", true)
+		admob.loadInterstitial("ca-app-pub-3940256099942544/1033173712")
+
 	if main.new_game:
 		hide_menu()
 	else:
 		show_menu(false)
 		get_node("Stage").get_tree().set_pause(true)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -31,10 +41,15 @@ func show_menu(_show_continue):
 		get_node("MainMenu/CanvasLayer/ContinueGameTextureButton").hide()
 	get_node("MainMenu/CanvasLayer/ExitGameTextureButton").show()
 
+	if admob:
+		admob.showBanner()
+
 func hide_menu():
 	get_node("MainMenu/CanvasLayer/NewGameTextureButton").hide()
 	get_node("MainMenu/CanvasLayer/ContinueGameTextureButton").hide()
 	get_node("MainMenu/CanvasLayer/ExitGameTextureButton").hide()
+	if admob:
+		admob.hideBanner()
 
 func _on_MainMenu_start_game():
 	hide_menu()
@@ -49,8 +64,14 @@ func _on_Stage_stage_pause():
 
 func _on_MainMenu_exit_game():
 	main.new_game = false
-	get_tree().quit()
+	if admob:
+		admob.showInterstitial()
+	else:
+		get_tree().quit()
 
+func _on_interstitial_close():
+	print("Interstitial closed")
+	get_tree().quit()
 
 func _on_MainMenu_new_game():
 	get_tree().reload_current_scene()
